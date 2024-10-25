cb8f2d289c21e537fa25731ff834729ea84ec60d
MariuszJM <mariusz.michna.j@gmail.com>
2024-10-25 12:13:31 +0200

    Refactor config.py to use default values for LLM_MODEL and LLM_PROVIDER environment variables

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 load_dotenv()
 
-LLM_MODEL = os.getenv("LLM_MODEL")
-LLM_PROVIDER = os.getenv("LLM_PROVIDER")
+LLM_MODEL = os.getenv("LLM_MODEL", "llama3.2:latest")
+LLM_PROVIDER = os.getenv("LLM_PROVIDER", "ollama")
 LLM_TEMPERATURE = int(os.getenv("LLM_TEMPERATURE", 0))
 LLM_MAX_TOKENS = int(os.getenv("LLM_MAX_TOKENS", 7500))
 OUTPUT_FOLDER = os.getenv(
